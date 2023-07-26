311a1335b7505d5964912c850b709a2344f35c48
red512 <robojinx1@gmail.com>
2023-07-26 14:23:15 +0300

    postgres and pv fix

--- terraform/helm-postrges.tf
+++ terraform/helm-postrges.tf
@@ -1,3 +1,22 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.default.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+#       command     = "aws"
+#     }
+#   }
+# }
+
+
 resource "helm_release" "postgres" {
   name             = "postgres"
   repository       = "https://charts.bitnami.com/bitnami"
@@ -11,6 +30,24 @@ resource "helm_release" "postgres" {
     value = "testdb"                          # Replace "my_database" with the name of your PostgreSQL database
   }
   values = [
-    file("values/postgres-values.yaml")
+    file("postgres-values.yaml")
   ]
 }
+
+resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
+  metadata {
+    name      = "postgres-pvc"
+    namespace = "postgres"
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "1Gi" # Adjust the storage size as per your requirements
+      }
+    }
+  }
+}
+
